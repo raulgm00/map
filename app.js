4c17074f0a9ab7b6a611ac57a74e5afbb6ebbaf8
ctd-frontend <92804884+ctd-frontend@users.noreply.github.com>
2022-09-26 18:12:02 -0300

    upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const listado = [{
+        nombre: 'Juan',
+        edad: 32
+    },
+    {
+        nombre: 'Lucía',
+        edad: 45
+    },
+    {
+        nombre: 'Nicolas',
+        edad: 22
+    }
+];
+
+const nodoUl = document.querySelector('ul');
+
+function mostrarPesonas(lista) {
+
+    lista.forEach(item => {
+    nodoUl.innerHTML += `<li><strong>${item.nombre}</stron> (${item.edad} años)</li>`        
+    });
+}
+
+mostrarPesonas(listado);
+
+/* -------------------------------- Consigna -------------------------------- */
+// Realizar el mismo proceso de renderizar en pantalla pero utilizando la función .map()
+// Detalles:
+// . indagar en la documentacion de MDN -> https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+// . comentar la función anterior y ejecutar esta para probar el resultado 👇
+
+function mostrarPesonasConMap(lista) {
+
+    lista.map();
+}
+
